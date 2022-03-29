0ff4e192c2661d4bbb7ce23ae0eb1c68c4179628
Muhammad Sabtain <sabtain40@gmail.com>
2022-03-29 10:52:56 +0500

    hash iterating example 1

---
+++ iterating_over_hashes.rb
@@ -0,0 +1,9 @@
+# iterating over hashes with each
+
+person = {name: 'sabtain', age: '28', height: '6 ft', hair_color: 'black'}
+
+
+person.each do |key, value|
+  puts "sabtain,s #{key} is #{value}"
+end
+
